a6754e4c6e4b37575a6e9cfd1bc6d05f52c67fd1
Hisham Amery <hishamamery@gmail.com>
2020-07-09 20:25:21 -0600

    Everything working but local storage

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Weather-Control-Panel
+A Better way to View the Weather
+
+During this project I created a simple and easy to use weather application that returns both the current and five day forecast based on your city. It returns the
+temperature, humidity, wind speed, UV index, and visual icon detailing weather conditions that day. Previous weather data is stored in the browser local storage
+for easy access as well. 
+
+
+Author: Hisham Amery
+
+Technologies/Languages: HTML, CSS, JavaScript, Moment.js, jQuery
+
+Deployed Site:
+
+Link: https://hamery93.github.io/Work-Day-Planner/
+
+
+![quiz screen shot](https://github.com/hamery93/Work-Day-Planner/blob/master/Assets/appScreenshot.jpg)
--- script.js
+++ script.js
@@ -14,7 +14,8 @@ $(document).ready(function () {
 
         searchWeather(cityName);
 
-        
+
+
     })
 
 
@@ -40,23 +41,28 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
+
             var dateInfo = response.dt;
-            
+            console.log(dateInfo);
+            var currentDate = moment.unix(dateInfo).format("L");
+            console.log("current date:  " + currentDate);
+
             //Local storage here, use prior assisgment 
             $("#today").empty()
 
             var card = $("<div>").addClass("card")
-            var wind = $("<p>").addClass("card-text").text("windSpeed: " + response.wind.speed)
-            var temp = $("<p>").addClass("card-text").text("temperature: " + response.main.temp)
-            var humidity = $("<p>").addClass("card-text").text("humidity: " + response.main.humidity)
+            var wind = $("<p>").addClass("card-text").text("Wind Speed: " + response.wind.speed + " mph")
+            var temp = $("<p>").addClass("card-text").text("Temperature: " + response.main.temp + "  F")
+            var humidity = $("<p>").addClass("card-text").text("Humidity: " + response.main.humidity + " %")
+            var date = $("<p>").addClass("card-text").text("Current Date: " + currentDate)
             var cardBody = $("<div>").addClass("card-body")
             // var icon ---------this is just weather icon
 
-            cardBody.append(temp, wind, humidity) //put icon here when you  get it
+            cardBody.append(date, temp, wind, humidity) //put icon here when you  get it
             card.append(cardBody)
             $("#today").append(card)
             getForecast(cityName);
-            //UV index will need it's own function. not going to pass  in cityname, it'll be something else
+
 
             var lon = response.coord.lon;
             var lat = response.coord.lat;
@@ -82,25 +88,25 @@ $(document).ready(function () {
         }).then(function (response) {
             var uvFinal = response.value;
             console.log(uvFinal);
-          
+
             $("#today").append("UV Index:  ");
             var uvButton = $("<button>").text(uvFinal);
             $("#today").append(uvButton);
 
             if (uvFinal < 3) {
-                
+
                 uvButton.attr("class", "Green");
             } else if (uvFinal < 6) {
-                
+
                 uvButton.attr("class", "Yellow");
             } else if (uvFinal < 8) {
-                
+
                 uvButton.attr("class", "Orange");
             } else if (uvFinal < 11) {
-                
+
                 uvButton.attr("class", "Red");
             } else {
-                
+
                 uvButton.attr("class", "Purple");
             }
 
@@ -124,7 +130,12 @@ $(document).ready(function () {
             method: "GET"
         }).then(function (response) {
             console.log(response);
+            
+            
             $("#forecast").html("<h3 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+
+            
+
 
             for (var i = 0; i < response.list.length; i++) {
 
@@ -135,11 +146,13 @@ $(document).ready(function () {
                     var icon = response.list[i].weather[0].icon;
                     var showIcon = $("<img src=http://openweathermap.org/img/wn/" + icon + ".png />")
 
+
                     // var title come back to this
                     var temp = $("<p>").addClass("card-text").text("Temp   " + response.list[i].main.temp + "  F");
                     var humid = $("<p>").addClass("card-text").text("Humidity   " + response.list[i].main.humidity + "  %");
+                    var dateOne = $("<p>").addClass("card-text").text( moment.unix(response.list[i].dt).utc().format("L"))
 
-                    col.append(card.append(body.append(temp, humid, showIcon))) //put icon and title in here too
+                    col.append(card.append(body.append(dateOne, temp, humid, showIcon))) //put icon and title in here too
                     $("#forecast .row").append(col)
                 }
 
@@ -148,53 +161,3 @@ $(document).ready(function () {
         })
     }
 })
-
-
-
-
-
-
-
-
-
-
-
-
-// function uvIdex(lon, lat) {
-
-//     var queryURL = "https://api.openweathermap.org/data/2.5/uvi?appid=a474538ef7da914bc793b0a7b8480af5&lat=";
-//     var mid = "&lon=";
-//     var searchUV = queryURL + lat + mid + lon;
-//     console.log(searchUV);
-
-//     $.ajax({
-//         url: indexSearch,
-//         method: "GET"
-//     }).then(function (response) {
-//         var uvFinal = response.value;
-
-//         // should be able to compare float to the numbers, try it out
-//         // then append button with uvFinal printed to it
-//         $(".uvIndex").append("UV Index: ");
-//         var uvButton = $("<button>").text(uvFinal);
-//         $(".uvIndex").append(uvButton);
-//         // then style uvFinal button with below
-//         if (uvFinal < 3) {
-//             // IF RETURN IS 0-2 SYLE GREEN
-//             uvButton.attr("class", "uvGreen");
-//         } else if (uvFinal < 6) {
-//             // IF 3-5 STYLE YELLOW
-//             uvButton.attr("class", "uvYellow");
-//         } else if (uvFinal < 8) {
-//             // IF 6-7 STYLE ORANGE
-//             uvButton.attr("class", "uvOrange");
-//         } else if (uvFinal < 11) {
-//             // IF 8-10 STYLE RED
-//             uvButton.attr("class", "uvRed");
-//         } else {
-//             // IF 11+ STYLE VIOLET
-//             uvButton.attr("class", "uvPurple");
-//         }
-//     });
-// }
-// //     
